bc9fa817be087178781d3f4bf8c568c66cd2b082
Sudip Neupane <neupane.sudeep991@gmail.com>
2023-08-08 17:08:24 +0545

    vue-js source code

---
+++ vue-js-setup/vue-js-app/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <div style="padding:40px;">
+      <h1>
+        Hello from
+        <span style="color:#29558C">HELLO WORLD</span>
+        <span style="color:#AD5353">From Sudip</span>
+      </h1>
+      <p>
+        Sample page for testing.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
